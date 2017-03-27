5adafbf211b38485c1ca6e5ddcc41808f1f0f1e5
pvn03 <pvn03@yahoo.com>
2017-03-27 19:06:42 -0400

    update

---
+++ registration.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome First Name: <?php echo $_POST["name"]; ?><br>
+        Last Name: <?php echo $_POST["last_name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,16 @@
+<html>
+<head> 
+		<title> Quiz 2 - Pavan K. Kottapalli</title>
+		<link rel="stylesheet" href="styles.css" >
+	</head>
+<body>
+
+<form action="registration.php" method="post">
+First Name: <input type="text" name="name"><br>
+Last Name: <input type="text" name="last_name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
